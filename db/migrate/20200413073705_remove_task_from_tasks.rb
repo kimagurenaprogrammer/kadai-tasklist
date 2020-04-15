6fa9e198058dddb18508683b0ce2af6d3097e3d1
kimagurenaprogrammer <loveandhappiness23236@gmail.com>
2020-04-15 08:13:08 +0000

    AddStatusToTasks

--- db/migrate/20200413073705_remove_task_from_tasks.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTaskFromTasks < ActiveRecord::Migration[5.2]
-  def change
-    remove_column :tasks, :task, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_04_13_075151) do
+ActiveRecord::Schema.define(version: 2020_04_15_080429) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
